e2adff7ad737371c76f5c9876a37a477dafbd133
Daisukekambe <219789162+Daisukekambe481@users.noreply.github.com>
2026-03-14 16:13:13 +0400

    apply ratelimmiting to otp and login

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -8,14 +8,18 @@ import {
   forgotPassword,
   resetPassword,
 } from '../controllers/auth.controller.js'
-
+import {
+  loginLimiter,
+  otpLimiter,
+  registerLimiter,
+} from '../middleware/rateLimiter.js'
 const router = Router()
 
-router.post('/register', register)
-router.post('/verify-email', verifyEmail)
-router.post('/login', login)
-router.post('/refresh', refresh)
-router.post('/forgot-password', forgotPassword)
-router.post('/reset-password', resetPassword)
+router.post('/register',registerLimiter, register)
+router.post('/verify-email',otpLimiter,verifyEmail)
+router.post('/login',loginLimiter,login)
+router.post('/refresh',refresh)
+router.post('/forgot-password',otpLimiter,forgotPassword)
+router.post('/reset-password',otpLimiter,resetPassword)
 
 export default router
